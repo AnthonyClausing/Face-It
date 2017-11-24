b48fe2e7e0d42563fa886da3e649502f4a248e19
ODEit <jose.sabal@yahoo.com>
2017-11-24 09:21:41 -0500

    changed the login page look

--- client/components/login.js
+++ client/components/login.js
@@ -10,10 +10,35 @@ export default class Login extends Component {
     render(){
         return(
             <div id ='login-page'>
+            <div className = 'login-header'>
+                <h1>Face-It</h1>
+                <p>An interactive multiplayer webcam game</p>
+            </div> 
+            <div className = 'login-items'>
                 <input className = 'login-items' placeholder = 'name'></input>
                 <NavLink to='/home' className = 'login-items'>EnterGame</NavLink>
+            </div>
             </div>
         )
     }
     
 }
+
+// //text-align: center;
+// display: flex;
+// flex-direction: column;
+// justify-content: space-between;
+// align-items: center;
+// width: 100vw;
+// height: 100vh;
+// }
+
+// {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     padding: 1em;
+//     margin: .1em;
+//     border-radius: 10%;
+//     transition: all 1.5s ease-in;
+//     text-decoration: none;
