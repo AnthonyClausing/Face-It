5acf2fe4ebc2829adfb4434a3c463b3a822355be
ODEit <jose.sabal@yahoo.com>
2017-11-24 22:38:40 -0500

    added audio for play on login

--- client/components/login.js
+++ client/components/login.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Main from './main'
 import { NavLink } from 'react-router-dom'
+import ReactAudioPlayer from 'react-audio-player';
 
 export default class Login extends Component {
     constructor(props) {
         super();
     }
+
 
     render() {
         return (
@@ -17,10 +19,11 @@ export default class Login extends Component {
                 <div className='login-items'>
                     <input className='login-items' placeholder='name'></input>
                     <NavLink to='/home' className='login-items'>EnterGame</NavLink>
-                    <audio autoPlay ='true' controls>
-                        <source src="http://192.254.184.234/~tangh/01_admin_resources/Shimmer/ogg/Smile.ogg" type="audio/ogg"></source>
-                        <source src="http://192.254.184.234/~tangh/01_admin_resources/Shimmer/mp3/Smile.mp3" type="audio/mpeg"></source>
-                    </audio>
+                    <ReactAudioPlayer
+                        src="pokemon-black-white.mp3"
+                        autoPlay
+                        controls
+                    />
                 </div>
             </div>
         )
