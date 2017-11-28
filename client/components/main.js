2150049626ce0bd842dafbe9cb3f76217a31cfaf
Plucker <plucker18@gmail.com>
2017-11-28 09:39:35 -0500

    cleaned up spacing, indentation, comments

--- client/components/main.js
+++ client/components/main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import VideoFeed from './videoFeed';
 import io from 'socket.io-client';
-import {connectToSite, joinRoom, newRoom, createPeerConnection, doAnswer} from '../socket.js';
+import { createPeerConnection, doAnswer} from '../socket.js';
 
 const socket = io(window.location.origin);
 
@@ -39,169 +39,141 @@ export default class Main extends Component {
 
     componentDidMount() {
 
-      let videoSource;
-      if (navigator.mediaDevices) {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-          .then(this.handleVideoSource)
-          .catch(console.log);
-      }
+        let videoSource;
+        if (navigator.mediaDevices) {
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+                .then(this.handleVideoSource)
+                .catch(console.log);
+        }
 
-    socket.on('connect', () => {
-      console.log('Connected!, My Socket Id:', socket.id);
-    });
-    socket.on('roomTaken', (msg) => {
-      console.log(msg);
-      document.getElementById('roomTaken').innerHTML = msg;
-    });
-    socket.on('someoneJoinedTheRoom', () => {
-      console.log('someone joined');
-      this.isInitiator = true;
-      this.createPeerConnection(this.state, socket);
-      console.log('pc after someone joined:', this.pc);
-    });
+        socket.on('connect', () => {
+            console.log('Connected!, My Socket Id:', socket.id);
+        });
+        socket.on('roomTaken', (msg) => {
+            console.log(msg);
+            document.getElementById('roomTaken').innerHTML = msg;
+        });
+        socket.on('someoneJoinedTheRoom', () => {
+            console.log('someone joined');
+            this.isInitiator = true;
+            this.createPeerConnection(this.state, socket);
+            console.log('pc after someone joined:', this.pc);
+        });
 
-    socket.on('signal', message => {
-      if (message.type === 'offer') {
-        console.log('received offer:', message);
-        this.pc.setRemoteDescription(new RTCSessionDescription(message));
-        this.doAnswer(socket);
-        this.pc.onaddstream = e => {
-          console.log('onaddstream', e);
-          this.remoteStream = e.stream;
-          this.remote = window.URL.createObjectURL(this.remoteStream);
-          this.setState({ remoteVidSource: this.remote });
-          // this.setState({bridge: 'established'});
-
-        };
-      }
-      else if (message.type === 'answer') {
-        console.log('received answer:', message);
-        this.pc.setRemoteDescription(new RTCSessionDescription(message));
-        // when the other side added a media stream, show it on screen
-        this.pc.onaddstream = e => {
-          console.log('onaddstream', e);
-          this.remoteStream = e.stream;
-          this.remote = window.URL.createObjectURL(this.remoteStream);
-          this.setState({ remoteVidSource: this.remote });
-          // this.setState({bridge: 'established'});
-        };
-      }
-      else if (message.type === 'candidate') {
-        this.pc.addIceCandidate(
-        new RTCIceCandidate({
-            sdpMLineIndex: message.mlineindex,
-            candidate: message.candidate
-        })
-        );}
-    });
+        socket.on('signal', message => {
+            if (message.type === 'offer') {
+                console.log('received offer:', message);
+                this.pc.setRemoteDescription(new RTCSessionDescription(message));
+                this.doAnswer(socket);
+                this.pc.onaddstream = e => {
+                    console.log('onaddstream', e);
+                    this.remoteStream = e.stream;
+                    this.remote = window.URL.createObjectURL(this.remoteStream);
+                    this.setState({ remoteVidSource: this.remote });
+                };
+            }
+            else if (message.type === 'answer') {
+                console.log('received answer:', message);
+                this.pc.setRemoteDescription(new RTCSessionDescription(message));
+                // when the other side added a media stream, show it on screen
+                this.pc.onaddstream = e => {
+                    console.log('onaddstream', e);
+                    this.remoteStream = e.stream;
+                    this.remote = window.URL.createObjectURL(this.remoteStream);
+                    this.setState({ remoteVidSource: this.remote });
+                };
+            }
+            else if (message.type === 'candidate') {
+                this.pc.addIceCandidate(
+                    new RTCIceCandidate({
+                        sdpMLineIndex: message.mlineindex,
+                        candidate: message.candidate
+                    })
+                );
+            }
+        });
     }
 
-    roomTaken (msg) {
-      document.getElementById('roomTaken').innerHTML = msg;
-  }
-
-  handleNewRoom (event) {
-      event.preventDefault();
-      socket.emit('newRoom', event.target.newRoom.value, socket.id);
-      console.log('NEW ROOM', event.target.newRoom.value);
-      event.target.newRoom.value = '';
-  }
-
-  handleJoinRoom (event) {
-      event.preventDefault();
-      this.createPeerConnection(this.state);
-      console.log('pc after join room:', this.pc, this.state);
-      socket.emit('joinRoom', event.target.joinRoom.value);
-      event.target.joinRoom.value = '';
-  }
-    // roomTaken (msg) {
-    //     document.getElementById('roomTaken').innerHTML = msg;
-    // }
-
-    // handleNewRoom (event) {
-    //     event.preventDefault();
-    //     newRoom(this.state.socket, event.target.newRoom.value);
-    //     event.target.newRoom.value = '';
-    // }
-
-    // handleJoinRoom (event) {
-    //     event.preventDefault();
-    //     joinRoom(this.state.socket, event.target.joinRoom.value)
-    //     event.target.joinRoom.value = '';
-    // }
-    handleVideoSource (mediaStream) {
-        this.setState({userVidSource: window.URL.createObjectURL(mediaStream), userMediaObject: mediaStream});
+    roomTaken(msg) {
+        document.getElementById('roomTaken').innerHTML = msg;
     }
 
-    startGame (event) {
+    handleNewRoom(event) {
+        event.preventDefault();
+        socket.emit('newRoom', event.target.newRoom.value, socket.id);
+        console.log('NEW ROOM', event.target.newRoom.value);
+        event.target.newRoom.value = '';
+    }
+
+    handleJoinRoom(event) {
+        event.preventDefault();
+        this.createPeerConnection(this.state);
+        console.log('pc after join room:', this.pc, this.state);
+        socket.emit('joinRoom', event.target.joinRoom.value);
+        event.target.joinRoom.value = '';
+    }
+
+    handleVideoSource(mediaStream) {
+        this.setState({ userVidSource: window.URL.createObjectURL(mediaStream), userMediaObject: mediaStream });
+    }
+
+    startGame(event) {
         event.preventDefault();
         this.changeTargetEmotion();
-        this.setState({interval: setInterval(this.changeTargetEmotion, 1000), gameState: 'active', count: event.target.numRounds.value});
+        this.setState({ interval: setInterval(this.changeTargetEmotion, 1000), gameState: 'active', count: event.target.numRounds.value });
     }
 
-    changeTargetEmotion () {
+    changeTargetEmotion() {
         console.log(this.state.gameState);
-        if (this.state.count > 0){
-            this.setState({targetEmotion: this.state.emotions[Math.floor(Math.random() * this.state.emotions.length)], matching: false, count: this.state.count - 1});
-        }  else {
+        if (this.state.count > 0) {
+            this.setState({ targetEmotion: this.state.emotions[Math.floor(Math.random() * this.state.emotions.length)], matching: false, count: this.state.count - 1 });
+        } else {
             clearInterval(this.state.interval);
-            this.setState({gameState: 'stopped'});
+            this.setState({ gameState: 'stopped' });
         }
     }
 
-    matchedEmotion () {
-        this.setState({matching: true});
+    matchedEmotion() {
+        this.setState({ matching: true });
     }
 
     render () {
-    //     const videoJsOptions = {//SRC WILL BE UNIQUE TO EACH PERSON IN THE ROOM.
-		// 	autoplay: true,
-		// 	controls: false,
-    //             src: this.state.videoSource
-        // };
-        console.log('MAIN remote',this.state.remoteVidSource)
         return (
-            <div id = "single-player">
+            <div id="single-player">
                 <h1> This is the main </h1>
-
                 <form onSubmit={this.handleNewRoom}>
-                  <label>
-                    Create Room:
+                    <label>
+                        Create Room:
                     <input type="text" name="newRoom" />
-                  </label>
-                  <input type="submit" name="submitNew" />
+                    </label>
+                    <input type="submit" name="submitNew" />
                 </form>
                 <form onSubmit={this.handleJoinRoom}>
-                  <label>
-                    Join Room:
+                    <label>
+                        Join Room:
                     <input type="text" name="joinRoom" />
-                  </label>
-                  <label>
-                    Name:
+                    </label>
+                    <label>
+                        Name:
                     <input type="text" name="userName" />
-                  </label>
-                  <input type="submit" name="submitJoin" />
+                    </label>
+                    <input type="submit" name="submitJoin" />
                 </form>
-
                 {
                     this.state.userVidSource &&
 
-                <VideoFeed matchedEmotion={this.matchedEmotion} videoSource={this.state.userVidSource} target={this.state.targetEmotion} />
+                    <VideoFeed matchedEmotion={this.matchedEmotion} videoSource={this.state.userVidSource} target={this.state.targetEmotion} />
                 }
                 {
                     this.state.remoteVidSource &&
-                <VideoFeed remoteVidSource={this.state.remoteVidSource} />
+                    <VideoFeed remoteVidSource={this.state.remoteVidSource} />
                 }
-             
-
                 <div id="targetEmotion"> Target: {this.state.targetEmotion} </div>
-
                 <div id="success">
                     {
                         this.state.matching ? 'Success' : 'Failing'
                     }
                 </div>
-
                 <div id="gameControls">
                     <form onSubmit={this.startGame}>
                         <label>
@@ -209,25 +181,9 @@ export default class Main extends Component {
                             <input name="numRounds" type="text" />
                         </label>
                         <input id="startGame" type="submit" disabled={this.state.gameState === 'active'} value="Start Game" />
-
                     </form>
                 </div>
             </div>
         );
     }
 }
-      // <form onSubmit={this.handleNewRoom}>
-                //     <label> 
-                //         Create Room:
-                //         <input type='text' name='newRoom'/>
-                //     </label>
-                //     <input type='submit' name='submitNew'/>
-                //     <div id='roomTaken'></div>
-                // </form>
-                // <form onSubmit={this.handleJoinRoom}>
-                //     <label>
-                //         Join Room:
-                //         <input type='text' name='joinRoom'/>
-                //     </label>
-                //     <input type='submit' name='submitJoin'/>
-                // </form>
--- client/components/videoFeed.js
+++ client/components/videoFeed.js
@@ -185,27 +185,27 @@ class VideoFeed extends React.Component {
 		return (
 			<div className="the-video">
 				{
-					this.props.remoteVidSource ?
-					<video
-					width="400"
-					height="300"
-					id='remoteVidFeed'
-					src={this.props.remoteVidSource}
-					ref={(video) => { this.video = video }}					
-					autoPlay="true">
-				</video> :
-				<video
-				width="400"
-				height="300"
-				id={this.props.id}
-				src={this.props.videoSource}
-				autoPlay="true"
-				muted
-				ref={(video) => { this.video = video }} >
-			</video>
+					this.props.remoteVidSource
+						?
+						<video
+							width="400"
+							height="300"
+							id='remoteVidFeed'
+							src={this.props.remoteVidSource}
+							ref={(video) => { this.video = video }}
+							autoPlay="true">
+						</video>
+						:
+						<video
+							width="400"
+							height="300"
+							id={this.props.id}
+							src={this.props.videoSource}
+							autoPlay="true"
+							muted
+							ref={(video) => { this.video = video }} >
+						</video>
 				}
-			
-
 
 				<div id='virtualButtons'>
 					<img id='blueButton' src="/images/SquareBlue.png" />
@@ -230,221 +230,3 @@ class VideoFeed extends React.Component {
 }
 
 module.exports = VideoFeed;
-
-//window.ReactFacialFeatureTracker = ReactFacialFeatureTracker;
-
-
-// import React from 'react';
-// import getUserMedia from 'getusermedia';
-// import emotionClassifier from './models/emotionclassifier.js';
-// import emotionModel from './models/emotionmodel.js';
-// import pModel from './models/pmodel.js';
-// import clm from '../ClamTracker/build/clmtrackr.js';
-// import _ from 'lodash';
-// import PubSub from 'pubsub-js';
-
-// function fastAbs (value) {
-// 	return (value ^ (value >> 31)) - (value >> 31);
-// }
-
-// function differenceAccuracy(target, data1, data2) {
-// 	if (data1.length != data2.length) return null;
-// 	var i = 0;
-// 	while (i < (data1.length * 0.25)) {
-// 		var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
-// 		var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
-// 		var diff = threshold(fastAbs(average1 - average2));
-// 		target[4*i] = diff;
-// 		target[4*i+1] = diff;
-// 		target[4*i+2] = diff;
-// 		target[4*i+3] = 0xFF;
-// 		++i;
-// 	}
-// }
-
-// function threshold(value) {
-// 	return (value > 0x15) ? 0xFF : 0;
-// }
-
-// //  Cross-Browser Implementierung von der URL-Funktion, eher unwichtig
-// window.URL = window.URL ||
-// window.webkitURL ||
-// window.msURL ||
-// window.mozURL;
-
-// class VideoFeed extends React.Component {
-// 	state = {
-// 		emotion: { emotion: '' }
-// 	}
-
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.PubSub = props.PubSub || PubSub;
-// 		this.blend = this.blend.bind(this);
-// 		this.lastImageData;
-// 		this.virtualButtonPositions = [{color:'blue', x:0, y:0}, {color: 'red', x:32, y:0}]
-// 	}
-
-// 	componentDidMount() {
-// 		let ec = new emotionClassifier();
-// 		ec.init(emotionModel);
-// 		let emotionData = ec.getBlank();
-// 		this.ec = ec;
-
-// 		getUserMedia({ video : true}, this.getUserMediaCallback.bind(this) );
-
-// 		let ctrack = new clm.tracker({useWebGL : true});
-// 		ctrack.init(pModel);
-// 		this.ctrack = ctrack;
-
-// 		this.blendedCtx = this.blended.getContext('2d');
-// 		this.canvasCtx = this.canvas.getContext('2d');
-
-// 		let self = this;
-
-// 		this.video.addEventListener('canplay', (this.startVideo).bind(this), false);
-// 	}
-
-// 	shouldComponentUpdate(nextProps, nextState) {
-// 		if (this.state.emotion.emotion !== nextState.emotion.emotion) {
-// 			this.PubSub.publish('emotion.update', nextState.emotion);
-
-// 			return true;
-// 		}
-// 	}
-
-// 	getUserMediaCallback(err, stream ) {
-// 		this.video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-// 		this.video.play();
-// 	}
-
-// 	startVideo(){
-// 		//seems to work fine without calling play
-// //		this.video.play();
-// 		this.ctrack.start(this.video);
-// 		// start loop to draw face
-// 		this.drawLoop();
-// 	}
-
-// 	blend() {
-// 		var width = this.canvas.width;
-// 		var height = this.canvas.height;
-// 		// get webcam image data
-// 		var sourceData = this.canvasCtx.getImageData(0, 0, width, height);
-// 		// create an image if the previous image doesn’t exist
-// 		if (!this.lastImageData) this.lastImageData = this.canvasCtx.getImageData(0, 0, width, height);
-// 		// create a ImageData instance to receive the blended result
-// 		var blendedData = this.canvasCtx.createImageData(width, height);
-// 		// blend the 2 images
-// 		differenceAccuracy(blendedData.data, sourceData.data, this.lastImageData.data);
-// 		// draw the result in a canvas
-// 		this.blendedCtx.putImageData(blendedData, 0, 0);
-// 		// store the current webcam image
-// 		this.lastImageData = sourceData;
-// 	}
-
-// 	checkAreas() {
-// 		// loop over the button areas
-// 		for (var r=0; r<this.virtualButtonPositions.length; ++r) {
-// 			// get the pixels in a button area from the blended image
-// 			var blendedData = this.blendedCtx.getImageData(
-// 				this.virtualButtonPositions[r].x,
-// 				this.virtualButtonPositions[r].y,
-// 				32,
-// 				32
-// 			);
-// 			var i = 0;
-// 			var average = 0;
-// 			// loop over the pixels
-// 			while (i < (blendedData.data.length / 4)) {
-// 				// make an average between the color channel
-// 				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
-// 				++i;
-// 			}
-// 			// calculate an average between of the color values of the note area
-// 			average = Math.round(average / (blendedData.data.length / 4));
-// 			if (average > 10) {
-// 				// over a small limit, consider that a movement is detected
-// 				// do some action to indicated area touched
-// 				this.props.matchedEmotion();
-// 				console.log('detected');
-// 				document.getElementById(this.virtualButtonPositions[r].color + 'Button').style.cssText = "border: 3px solid yellow";
-// 			}
-// 		}
-// 	}
-
-// 	drawLoop(){
-// 		requestAnimationFrame((this.drawLoop).bind(this));
-    
-// 		let cp = this.ctrack.getCurrentParameters();
-
-// 		this.canvasCtx.clearRect(0, 0, 400, 300);
-// 		this.canvasCtx.drawImage(this.video, 0, 0, this.video.width, this.video.height);
-// 		this.blend();
-// 		this.checkAreas();
-
-// 		//this draws the wire face image on the canvas
-// 		// if (this.ctrack.getCurrentPosition()) {
-// 		// 	this.ctrack.draw(this.overlay);
-// 		// }
-
-// 		// Die Emotionen in darstellbare Form bringen
-// 		let er = this.ec.meanPredict(cp);
-// 		if (this.props.target === 'angry' && er[0].value > .5) {
-// 			this.props.matchedEmotion();
-// 		} else if (this.props.target === 'happy' && er[3].value > .5) {
-// 			this.props.matchedEmotion();
-// 		} else if (this.props.target === 'sad' && er[1].value > .5) {
-// 			this.props.matchedEmotion();
-// 		} else if (this.props.target === 'surprised' && er[2].value > .5) {
-// 			this.props.matchedEmotion();
-// 		}
-		
-//         document.getElementById('angry').innerHTML = '<span> Anger </span>' + er[0].value;
-//         document.getElementById('happy').innerHTML = '<span> Happy </span>' + er[3].value;
-//         document.getElementById('sad').innerHTML = '<span> Sad </span>' + er[1].value;
-//         document.getElementById('surprised').innerHTML = '<span> Surprised </span>' + er[2].value;
-
-// 		if (er) {
-// 			const emotion = _.maxBy(er, (o) => { return o.value; });
-// 			this.setState({ emotion: emotion });
-// 			this.PubSub.publish('emotions.loop', er);
-// 		}
-
-// 	}
-
-// 	render(props) {
-// 		return (
-// 			<div className="the-video">
-// 				<video
-// 					width="400"
-// 					height="300"
-// 					ref={ (video) => { this.video = video } } >
-// 				</video>
-
-// 				<div id='virtualButtons'>
-// 					<img id='blueButton' src="/images/SquareBlue.png" />
-// 					<img id='redButton' src="/images/SquareRed.png" />
-// 				</div>
-
-// 				<canvas id='canvas-source'
-// 					width="400"
-// 					height="300"
-// 					ref={ (canvas) => this.canvas = canvas }>
-// 				</canvas>
-
-// 				<canvas id='blended'
-// 					width="400"
-// 					height="300"
-// 					ref={ (canvas) => this.blended = canvas}>
-// 				</canvas>
-
-// 			</div>
-// 		)
-// 	}
-// }
-
-// module.exports = VideoFeed;
-
-// //window.ReactFacialFeatureTracker = ReactFacialFeatureTracker;
